9d5ce6b34fac9ce4fc8ccc9f40fe72bdc52e58c2
Krampuso <alexander.e.thompson000@gmail.com>
2025-01-27 13:13:53 -0500

    Player Movement Created

---
+++ SandSpade/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10f;
+    private Rigidbody2D rb;
+
+    private Vector2 movementDirection;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+   
+    }
+    
+    void FixedUpdate()
+    {
+        rb.velocity = movementDirection * speed;
+    }
+}
