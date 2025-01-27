1c461c22a06aa8707912c5f88f24c954990fd3c3
Krampuso <alexander.e.thompson000@gmail.com>
2025-01-27 14:24:10 -0500

    update

---
+++ SandSpade/Assets/Scripts/EnemyNavigation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNavigation : MonoBehaviour
+{
+    public float enemySpeed = 5f;
+    private Rigidbody2D rb;
+    
+    private bool flip = false;
+
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        flip = false;
+
+        if(flip == false){
+            rb.velocity = Vector2.right * enemySpeed;
+        }
+        if(flip == true){
+            rb.velocity = Vector2.right * -enemySpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.localScale = new Vector2 (-1, 1);
+        flip = true; 
+        Debug.Log("Flip is " + flip);
+       
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        flip = false;
+        Debug.Log("Flip is " + flip);
+    }
+}
