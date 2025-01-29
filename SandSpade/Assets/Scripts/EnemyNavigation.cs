8fe9271918c5c97801f2035e5344146a84903620
BilboBallins101 <153792351+BilboBallins101@users.noreply.github.com>
2025-01-28 20:35:46 -0500

    Changed EnemyNav and added Game Manager

--- SandSpade/Assets/Scripts/EnemyNavigation.cs
+++ SandSpade/Assets/Scripts/EnemyNavigation.cs
@@ -4,45 +4,38 @@ using UnityEngine;
 
 public class EnemyNavigation : MonoBehaviour
 {
-    public float enemySpeed = 5f;
-    private Rigidbody2D rb;
-    
-    public GameObject target;
-    private bool flip = false;
+    public float enemySpeed = 5f; // Speed of the enemy
+    private Rigidbody2D rb; // Ridgidbody reference
+    private bool movingRight = true; // Tracks the enemy's direction
 
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();  
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        flip = false;
-        //rb.velocity = Vector2.MoveTowards(transform.position, targetpos, 0.0f);
-
-        /*if(flip == false){
+        // Move the enemy based on direction
+        if (movingRight)
+        {
             rb.velocity = Vector2.right * enemySpeed;
-        }*/
+        }
+        else
+        {
+            rb.velocity = Vector2.left * enemySpeed;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.localScale = new Vector2 (-1, 1);
-        flip = true; 
-        Debug.Log("Flip is " + flip);
-       
-    }
+        // Flip direction
+        movingRight = !movingRight;
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        flip = false;
-        Debug.Log("Flip is " + flip);
-    }
+        // Flip sprite
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
 
+        Debug.Log("Flipped direction. Moving right: " + movingRight);
+    }
 }
