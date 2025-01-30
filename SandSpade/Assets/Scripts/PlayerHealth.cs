34a27f7144c82f5bd1488fc761529c9903a597ba
Dalton Norris <153792351+BilboBallins101@users.noreply.github.com>
2025-01-30 18:31:04 -0500

    Update 01/30
    Improved enemy nav, Added player health, Added buried items, changed scene.

---
+++ SandSpade/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int lives = 3; // Player starts with 3 lives
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) // Check if colliding with an enemy
+        {
+            LoseLife();
+        }
+        else if (collision.gameObject.CompareTag("Buried Item")) // Check if colliding with a Buried item
+        {
+            LoseLife();
+        }
+    }
+
+    void LoseLife()
+    {
+        lives--; // Reduce lives by 1
+        Debug.Log("Player hit! Lives remaining: " + lives);
+
+        if (lives <= 0)
+        {
+            Destroy(gameObject); // Destroy player when lives reach 0
+            Debug.Log("Player is out of lives!");
+        }
+    }
+}
