139090c83c9a07db984edb906c053da4f210cef8
Krampuso <al849783@ucf.edu>
2025-01-31 18:19:02 -0500

    Working On attack script

--- SandSpade/Assets/Scripts/PlayerAttack.cs
+++ SandSpade/Assets/Scripts/PlayerAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {   
     public PlayerController player;
+    public float pumpDistance = 3.0f;
+    public LayerMask attackLayer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,16 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            LaunchAttack();
+        }
+    }
+
+    void LaunchAttack()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, pumpDistance,attackLayer);
+        Debug.DrawRay(transform.position, Vector2.right * pumpDistance, Color.green);
     }
 }
+
