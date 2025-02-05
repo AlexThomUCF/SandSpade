6d861ad195a8b560ce79f6a710ce824d55ea762f
BilboBallins101 <153792351+BilboBallins101@users.noreply.github.com>
2025-02-04 22:24:02 -0500

    Working on UI

--- SandSpade/Assets/Scripts/PlayerHealth.cs
+++ SandSpade/Assets/Scripts/PlayerHealth.cs
@@ -1,24 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     PlayerController player;
- 
     public int lives = 3; // Player starts with 3 lives
+    public List<RawImage> lifeIcons; // Use RawImage instead of Image
+
     void Start()
     {
         player = GetComponent<PlayerController>();
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy")) // Check if colliding with an enemy
-        {
-            LoseLife();
-        }
-        else if (collision.gameObject.CompareTag("Buried Item")) // Check if colliding with a Buried item
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Buried Item"))
         {
             LoseLife();
         }
@@ -28,6 +27,12 @@ public class PlayerHealth : MonoBehaviour
     {
         lives--; // Reduce lives by 1
         Debug.Log("Player hit! Lives remaining: " + lives);
+
+        if (lifeIcons.Count > 0) 
+        {
+            lifeIcons[lifeIcons.Count - 1].gameObject.SetActive(false); // Hide the last life icon
+            lifeIcons.RemoveAt(lifeIcons.Count - 1); // Remove reference
+        }
 
         if (lives <= 0)
         {
@@ -42,7 +47,6 @@ public class PlayerHealth : MonoBehaviour
 
         Destroy(gameObject); // Destroy player when lives reach 0
         Debug.Log("Player is out of lives!");
-
         SceneManager.LoadScene("GameOver");
     }
 }
